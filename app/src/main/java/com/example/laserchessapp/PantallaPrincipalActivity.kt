6c91e7825c27cc740de892cd5b80ae6a7e3383ec
Jorge Sebastián <901931@unizar.es>
2026-02-18 20:50:33 +0100

    Pantalla principal con boton de navegación simple Closes #2

---
+++ app/src/main/java/com/example/laserchessapp/PantallaPrincipalActivity.kt
@@ -0,0 +1,30 @@
+package com.example.laserchessapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PantallaPrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pantalla_principal)
+        val btnPantalla2 = findViewById<Button>(R.id.btnPantalla2)
+        btnPantalla2.setOnClickListener { navigateToPantalla2() }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun navigateToPantalla2(){
+        val intent = Intent(this, Pantalla2Activity::class.java)
+        startActivity(intent)
+    }
+}
