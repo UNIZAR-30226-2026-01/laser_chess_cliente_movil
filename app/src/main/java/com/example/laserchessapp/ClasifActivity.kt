99f8e32e73af0d4a2796b0eef1d612048ea06201
Jorge Sebastián <901931@unizar.es>
2026-02-24 18:44:35 +0100

    Barra de navegación creada y funcional para todas las pantallas, agregados boton de opciones, jugar, clasificacion, usuario y monedas con sus respectivos iconos.

--- app/src/main/java/com/example/laserchessapp/ClasifActivity.kt
+++ app/src/main/java/com/example/laserchessapp/ClasifActivity.kt
@@ -1,6 +1,8 @@
 package com.example.laserchessapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,40 @@ class ClasifActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_clasif)
+
+        val btnTiendaActivity = findViewById<ImageButton>(R.id.btnTienda)
+        val btnPersoActivity = findViewById<ImageButton>(R.id.btnPerso)
+        val btnSocialActivity = findViewById<ImageButton>(R.id.btnSocial)
+        val btnHomeActivity = findViewById<ImageButton>(R.id.btnHome)
+        btnTiendaActivity.setOnClickListener { navigateToTienda() }
+        btnPersoActivity.setOnClickListener { navigateToPerso() }
+        btnSocialActivity.setOnClickListener { navigateToSocial() }
+        btnHomeActivity.setOnClickListener { navigateToHome() }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun navigateToTienda(){
+        val intent = Intent(this, TiendaActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun navigateToPerso(){
+        val intent = Intent(this, PersoActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun navigateToSocial(){
+        val intent = Intent(this, SocialActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun navigateToHome(){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
