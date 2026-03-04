ca395eacae61affe96d9ba303520803a41e584eb
Davih85 <897745@unizar.es>
2026-03-04 23:28:14 +0100

    Lógica para el inicio de sesion y registro de cuentas

---
+++ app/src/main/java/com/example/laserchessapp/AccountModels.kt
@@ -0,0 +1,35 @@
+package com.example.laserchessapp.network
+
+//Para definir los datos que se envian
+
+// Para login (los mismos de LoginDTO)
+data class LoginRequest(
+    val credential: String, //email/username
+    val password: String
+)
+
+// Respuesta del login
+data class LoginResponse(
+    val access_token: String
+)
+
+// Registro (los mismos de CreateAccountDTO)
+data class RegisterRequest(
+    val username: String,
+    val mail: String,
+    val password: String
+)
+
+// Respuesta del registro
+data class AccountResponse(
+    val account_id: Long
+)
+
+// Para actualizar cuenta
+data class UpdateAccountRequest(
+    val username: String? = null,
+    val mail: String? = null,
+    val board_skin: Int? = null,
+    val piece_skin: Int? = null,
+    val win_animation: Int? = null
+)
