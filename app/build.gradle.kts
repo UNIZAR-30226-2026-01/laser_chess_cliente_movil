b8d1f1b1887209c47ce26f4525453bd1fa6657a5
ainhoacrb <901808@unizar.es>
2026-03-09 00:14:53 +0100

    Arreglo conflictos al mergear con local

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.gracehopper.laserchessapp"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.gracehopper.laserchessapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
