ed069950c9c4e6fd9b7f31926b16bb5c6e9f1d2d
Jorge Sebastián <901931@unizar.es>
2026-03-11 12:22:19 +0100

    Arreglados bug con gradle (comentados todas las referencias a compose [Color.kt, Theme.kt, Type.kt]) Proyecto compilable

--- app/src/main/java/com/gracehopper/laserchessapp/ui/game/board/Board.kt
+++ app/src/main/java/com/gracehopper/laserchessapp/ui/game/board/Board.kt
@@ -1,10 +1,10 @@
 package com.gracehopper.laserchessapp.ui.game.board
 
-import com.gracehopper.laserchessapp.ui.game.pieces.Piece
-
-class Board {
-    private val _pieces = mutableListOf<Piece>()
-    val pieces get() = _pieces.toList()
-
-
-}
+//import com.gracehopper.laserchessapp.ui.game.pieces.Piece
+//
+//class Board {
+//    private val _pieces = mutableListOf<Piece>()
+//    val pieces get() = _pieces.toList()
+//
+//
+//}
