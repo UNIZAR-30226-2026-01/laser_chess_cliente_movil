b55de3a9468312127485356aa44817fc1c7fe203
Jorge Sebastián <901931@unizar.es>
2026-03-11 15:02:59 +0100

    Pequeños cambios en botones

--- app/src/main/java/com/gracehopper/laserchessapp/ui/game/GameActivity.kt
+++ app/src/main/java/com/gracehopper/laserchessapp/ui/game/GameActivity.kt
@@ -1,6 +1,7 @@
 package com.gracehopper.laserchessapp.ui.game
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.gracehopper.laserchessapp.R
 
@@ -9,5 +10,11 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        val btnExit = findViewById<ImageButton>(R.id.btnExit)
+
+        btnExit.setOnClickListener {
+            finish()
+        }
     }
 }
