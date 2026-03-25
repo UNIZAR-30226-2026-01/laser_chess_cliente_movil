98f278451e1f7203b8adb0a1715be1b1b278f6c5
ainhoacrb <901808@unizar.es>
2026-03-26 00:52:48 +0100

    DTO definido y acordado entre backend-frontend correctamente (Closes #48)

--- app/src/main/java/com/gracehopper/laserchessapp/data/model/ranking/AllRatingsResponse.kt
+++ app/src/main/java/com/gracehopper/laserchessapp/data/model/ranking/AllRatingsResponse.kt
@@ -2,6 +2,14 @@ package com.gracehopper.laserchessapp.data.model.ranking
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Clase de datos para obtener todos los ratings del usuario
+ * @property userId Id del usuario
+ * @property blitz Rating de blitz
+ * @property rapid Rating de rapid
+ * @property classic Rating de classic
+ * @property extended Rating de extended
+ */
 data class AllRatingsResponse (
     @SerializedName("user_id") val userId: String,
     @SerializedName("blitz") val blitz: Int,
