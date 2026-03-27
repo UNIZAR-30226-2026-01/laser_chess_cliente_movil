abb44136443b9f4b34892e52e05efa934f2be8eb
ainhoacrb <901808@unizar.es>
2026-03-27 02:32:15 +0100

    Lógica de social, requests, userProfile separada

---
+++ app/src/main/java/com/gracehopper/laserchessapp/ui/social/SocialFragment.kt
@@ -0,0 +1,280 @@
+package com.gracehopper.laserchessapp.ui.social
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.gracehopper.laserchessapp.R
+import com.gracehopper.laserchessapp.data.model.game.BoardType
+import com.gracehopper.laserchessapp.data.model.game.InProgressMatchSummary
+import com.gracehopper.laserchessapp.data.model.social.FriendSummary
+import com.gracehopper.laserchessapp.data.model.user.TimeMode
+import com.gracehopper.laserchessapp.data.remote.NetworkUtils
+import com.gracehopper.laserchessapp.data.repository.FriendRepository
+import com.gracehopper.laserchessapp.databinding.FragmentSocialBinding
+import com.gracehopper.laserchessapp.ui.FriendAdapter
+import com.gracehopper.laserchessapp.ui.InProgressAdapter
+import com.gracehopper.laserchessapp.ui.user.UserProfileDialogFragment
+
+class SocialFragment : Fragment() {
+
+    private var _binding: FragmentSocialBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var friendsAdapter: FriendAdapter
+    private lateinit var emptyMessage: TextView
+    private lateinit var recyclerFriends : RecyclerView
+
+    private lateinit var inProgressAdapter: InProgressAdapter
+
+    private val repository by lazy {
+        FriendRepository(NetworkUtils.getApiService())
+    }
+
+    private enum class SocialTab {
+        SOCIAL, IN_PROGRESS
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSocialBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        emptyMessage = view.findViewById(R.id.emptyMessage)
+        recyclerFriends = view.findViewById(R.id.recyclerFriends)
+
+        setupRecycler()
+        loadFriends()
+        loadFakeGamesInProgress()
+        setupTabs()
+
+        // cada vez que el dialogo se cierre, se vuelve a cargar la lista de amigos
+        parentFragmentManager.setFragmentResultListener(
+            "requests_dialog_closed",
+            viewLifecycleOwner
+        ) { _, _ ->
+            loadFriends()
+        }
+
+        // cada vez que se elimine un amigo, se vuelve a cargar la lista de amigos
+        parentFragmentManager.setFragmentResultListener(
+            "friend_removed",
+            viewLifecycleOwner
+        ) { _, _ ->
+            loadFriends()
+        }
+
+        setupListeners()
+        selectTab(SocialTab.SOCIAL)
+    }
+
+    private fun showUserProfileDialog(friend: FriendSummary) {
+        UserProfileDialogFragment.newInstance(friend.id)
+            .show(parentFragmentManager, "UserProfileDialog")
+    }
+
+    private fun setupRecycler() {
+        friendsAdapter = FriendAdapter(emptyList()) { friend -> showUserProfileDialog(friend) }
+        binding.recyclerFriends.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = friendsAdapter
+        }
+
+        inProgressAdapter = InProgressAdapter(emptyList()) { game -> resumeGame(game) }
+        binding.recyclerInProgressGames.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = inProgressAdapter
+        }
+    }
+
+    private fun loadFriends() {
+
+        repository.getFriends(onSuccess = { friends ->
+            if (friends != null) {
+                if (friends.isEmpty()) {
+                    emptyMessage.visibility = View.VISIBLE
+                } else {
+                    friendsAdapter.updateFriends(friends)
+                    emptyMessage.visibility = View.GONE
+                }
+            }
+        }, onError = { errorCode ->
+            emptyMessage.visibility = View.VISIBLE
+
+            when (errorCode) {
+                401 -> Toast.makeText(requireContext(), "No autorizado", Toast.LENGTH_SHORT).show()
+                500 -> Toast.makeText(requireContext(), "Error del servidor", Toast.LENGTH_SHORT).show()
+                null -> Toast.makeText(requireContext(), "Error de conexión", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(requireContext(), "Error: $errorCode", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun loadFakeGamesInProgress() {
+        val fakeMatches = listOf(
+            InProgressMatchSummary(
+                id = "1",
+                myTime = "13:00",
+                opponentUsername = "Usuario",
+                opponentTime = "12:00",
+                timeMode = TimeMode.BLITZ,
+                boardType = BoardType.ACE
+            ),
+            InProgressMatchSummary(
+                id = "2",
+                myTime = "14:00",
+                opponentUsername = "Usuario",
+                opponentTime = "11:00",
+                timeMode = TimeMode.EXTENDED,
+                boardType = BoardType.SOPHIE
+            )
+        )
+
+        if (fakeMatches.isEmpty()) {
+            binding.emptyInProgressMessage.visibility = View.VISIBLE
+            binding.recyclerInProgressGames.visibility = View.GONE
+        } else {
+            binding.emptyInProgressMessage.visibility = View.GONE
+            binding.recyclerInProgressGames.visibility = View.VISIBLE
+            inProgressAdapter.updateData(fakeMatches)
+        }
+    }
+
+    private fun resumeGame(game: InProgressMatchSummary) {
+        Toast.makeText(requireContext(), "Retomar partida con id ${game.id}", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setupTabs() {
+        binding.tabSocial.setOnClickListener { selectTab(SocialTab.SOCIAL) }
+
+        binding.tabInProgress.setOnClickListener { selectTab(SocialTab.IN_PROGRESS) }
+    }
+
+    private fun selectTab(tab: SocialTab) {
+        when (tab) {
+            SocialTab.SOCIAL -> {
+                binding.layoutSocialContent.visibility = View.VISIBLE
+                binding.btnAddFriend.visibility = View.VISIBLE
+                binding.layoutInProgressContent.visibility = View.GONE
+
+                binding.tabSocial.setBackgroundResource(R.drawable.bg_tab_selected)
+                binding.tabInProgress.setBackgroundResource(R.drawable.bg_tab_unselected)
+            }
+
+            SocialTab.IN_PROGRESS -> {
+                binding.layoutSocialContent.visibility = View.GONE
+                binding.btnAddFriend.visibility = View.GONE
+                binding.layoutInProgressContent.visibility = View.VISIBLE
+
+                binding.tabSocial.setBackgroundResource(R.drawable.bg_tab_unselected)
+                binding.tabInProgress.setBackgroundResource(R.drawable.bg_tab_selected)
+            }
+        }
+    }
+
+    private fun setupListeners() {
+        binding.btnAddFriend.setOnClickListener {
+            showAddFriendDialog()
+        }
+        binding.cardSolicitudes.setOnClickListener {
+            DialogRequestsFragment().show(parentFragmentManager, "DialogRequests")
+        }
+    }
+
+    private fun showAddFriendDialog() {
+        val dialogView = LayoutInflater.from(requireContext())
+            .inflate(R.layout.dialog_add_friend, null)
+
+        val editTextUsername = dialogView.findViewById<EditText>(R.id.editTextFriendUsername)
+        val buttonSendFriendRequest = dialogView.findViewById<Button>(R.id.buttonSendFriendRequest)
+        val buttonCopyInvitationLink = dialogView.findViewById<Button>(R.id.buttonCopyInvitationLink)
+        val buttonCloseDialog = dialogView.findViewById<ImageButton>(R.id.buttonCloseDialog)
+        val textInvitationLink = dialogView.findViewById<TextView>(R.id.textInvitationLink)
+
+        val invitationLink = "https://www.reddit.com/r/adventuretime/comments/1g9hv45/i_love_adventure_time_so_much_i_cant_even_express/?tl=pt-br"
+
+        textInvitationLink.text = invitationLink
+
+        val dialog = AlertDialog.Builder(requireContext())
+            .setView(dialogView)
+            .create()
+
+        textInvitationLink.setOnClickListener {
+            copyInvitationLink(invitationLink)
+        }
+
+        buttonCopyInvitationLink.setOnClickListener {
+            copyInvitationLink(invitationLink)
+        }
+
+        buttonSendFriendRequest.setOnClickListener {
+            val username = editTextUsername.text.toString().trim()
+
+            if (username.isNotEmpty()) {
+                sendFriendRequest(username)
+                dialog.dismiss()
+            } else {
+                Toast.makeText(requireContext(),
+                    "Por favor, introduce un nombre de usuario",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        buttonCloseDialog.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    fun copyInvitationLink(invitationLink: String) {
+        val clipboard = requireContext().getSystemService(Context.CLIPBOARD_SERVICE)
+                as ClipboardManager
+        val clip = ClipData.newPlainText("invitation_link", invitationLink)
+        clipboard.setPrimaryClip(clip)
+
+        Toast.makeText(requireContext(), "Enlace copiado", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun sendFriendRequest(username : String) {
+
+        repository.addFriend(username = username, onSuccess = {
+            Toast.makeText(requireContext(), "Solicitud enviada a $username", Toast.LENGTH_SHORT).show()
+
+            loadFriends()
+        }, onError = { errorCode ->
+            when (errorCode) {
+                400 -> Toast.makeText(requireContext(), "Solicitud inválida", Toast.LENGTH_SHORT).show()
+                401 -> Toast.makeText(requireContext(), "No autorizado", Toast.LENGTH_SHORT).show()
+                404 -> Toast.makeText(requireContext(), "Usuario no encontrado", Toast.LENGTH_SHORT).show()
+                409 -> Toast.makeText(requireContext(), "Ya existe una amistad o solicitud", Toast.LENGTH_SHORT).show()
+                null -> Toast.makeText(requireContext(), "Error de conexión", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(requireContext(), "Error: $errorCode", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
