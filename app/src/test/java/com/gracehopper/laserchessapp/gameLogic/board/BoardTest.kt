7a4ce38dcdbd3ceeee97db45210482c48947d6ab
Jorge Sebastian <901931@unizar.es>
2026-04-12 14:22:36 +0200

    Test del tablero hecho

---
+++ app/src/test/java/com/gracehopper/laserchessapp/gameLogic/board/BoardTest.kt
@@ -0,0 +1,72 @@
+package com.gracehopper.laserchessapp.gameLogic.board
+
+import com.gracehopper.laserchessapp.gameLogic.pieces.Piece
+import com.gracehopper.laserchessapp.gameLogic.pieces.PieceType
+import org.junit.Assert.*
+import org.junit.Test
+
+class BoardTest {
+
+    /**
+     * TEST 1: Poner pieza y obtenerla
+     */
+    @Test
+    fun set_and_get_piece() {
+        val board = Board(10, 8)
+        val piece = Piece(true, PieceType.DEFLECTOR)
+
+        board.setPiece(5, 4, piece)
+
+        val result = board.getPiece(5, 4)
+
+        assertEquals(piece, result)
+    }
+
+    /**
+     * TEST 2: Obtener celda vacía da null
+     */
+    @Test
+    fun get_empty_cell_returns_null() {
+        val board = Board(10, 8)
+
+        val result = board.getPiece(3, 3)
+
+        assertNull(result)
+    }
+
+    /**
+     * TEST 4: Celda prohibida roja
+     */
+    @Test
+    fun forbidden_cell_red_piece() {
+        val board = Board(10, 8)
+
+        val result = board.isForbiddenCell(0, 3, true)
+
+        assertTrue(result)
+    }
+
+    /**
+     * TEST 5: Celda prohibida azul
+     */
+    @Test
+    fun forbidden_cell_blue_piece() {
+        val board = Board(10, 8)
+
+        val result = board.isForbiddenCell(9, 3, false)
+
+        assertTrue(result)
+    }
+
+    /**
+     * TEST 6: Celda permitida
+     */
+    @Test
+    fun allowed_cell() {
+        val board = Board(10, 8)
+
+        val result = board.isForbiddenCell(5, 4, true)
+
+        assertFalse(result)
+    }
+}
